437f4d3fafae6cd08807f7e41868dbd628e1994f
devfiona <87800899+devfiona@users.noreply.github.com>
2024-12-01 15:01:25 +0100

    chore: add mobile menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,28 +1,72 @@
+import { useState } from "react";
 import logo from "../assets/amanahwhite.png";
 import Menu from "../assets/svgs/Menu";
+import Close from "../assets/svgs/Close";
+
+const navLinks = [
+  {
+    route: "#",
+    name: "Home",
+  },
+  {
+    route: "#about",
+    name: "About",
+  },
+  {
+    route: "#features",
+    name: "Features",
+  },
+  {
+    route: "#how-it-works",
+    name: "How it Works",
+  },
+];
 
 const Navigation = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="px-4 py-6 sticky top-0 left-0 w-full bg-black-50 flex justify-between items-start shadow-sm md:px-10 lg:px-20 lg:items-center xl:px-[7.5rem]">
       <div className="w-fit">
         <img src={logo} alt="Amanah Logo" className="w-3/5 lg:w-4/5" />
       </div>
-      <button className="cursor-pointer lg:hidden">
-        <Menu />
-      </button>
+      <div>
+        <button
+          className="cursor-pointer lg:hidden"
+          onClick={() => setOpen(true)}
+        >
+          <Menu />
+        </button>
+        <div
+          className={`pl-12 pr-6 py-6 bg-black-50 h-screen w-full absolute top-0 left-0 z-20 transition-transform ${
+            open ? "-translate-x-0" : "-translate-x-[500px]"
+          }`}
+        >
+          <button onClick={() => setOpen(false)} className="flex ml-auto">
+            <Close className="w-8 h-8" />
+          </button>
+          <div className="pt-6 flex flex-col justify-between">
+            <ul className="pt-10 grid gap-8">
+              {navLinks?.map(({ route, name }) => (
+                <li key={name}>
+                  <a href={route} className="text-2xl">
+                    {name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <button className="w-full px-6 py-3 text-black-50 font-semibold bg-primary-100 cursor-pointer rounded-lg mt-32 mr-6 text-lg">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
       <ul className="hidden lg:flex lg:gap-4 lg:font-normal xl:gap-6">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#features">Features</a>
-        </li>
-        <li>
-          <a href="#how-it-works">How it Works</a>
-        </li>
+        {navLinks?.map(({ route, name }) => (
+          <li key={name}>
+            <a href={route}>{name}</a>
+          </li>
+        ))}
       </ul>
       <button className="hidden lg:block px-6 py-2.5 text-black-50 font-medium bg-primary-100 cursor-pointer rounded-lg transition duration-300 xl:hover:bg-primary-200">
         Get Started
