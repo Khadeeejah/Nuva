03bdc01d0772e2f76a9eb3a89329c2e1a0b9dee6
devfiona <87800899+devfiona@users.noreply.github.com>
2024-12-04 17:28:48 +0100

    fix: design bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,28 +4,30 @@ const About = () => {
       <div className="flex flex-col gap-14 lg:flex-row xl:gap-24">
         <div className="lg:w-3/5">
           <h2 className="text-[1.688rem] text-center font-semibold leading-8 md:text-3xl lg:text-4xl lg:text-left lg:leading-10 xl:text-5xl xl:leading-tight">
-          The Future of Insurance Is Here
+            The Future of Insurance Is Here
           </h2>
         </div>
         <div className="grid gap-6 text-center text-neutral-100 lg:text-left lg:w-3/4 xl:text-lg">
           <p>
             Nuva combines cutting-edge{" "}
-            <span className="text-primary-100 font-semibold"> AI</span>{" "} and {" "}
-            <span className="text-primary-100 font-semibold">Blockchain technology </span>{" "}
-            that provides a comprehensive and customisable technology platform
-             to create an inclusive, efficient, and transparent insurance ecosystem while allowing 
-             full control over data and compliance.
-
+            <span className="text-primary-100 font-semibold"> AI</span> and{" "}
+            <span className="text-primary-100 font-semibold">Blockchain </span>{" "}
+            technology that provides a comprehensive and customisable technology
+            platform to create an inclusive, efficient, and transparent
+            insurance ecosystem while allowing full control over data and
+            compliance.
           </p>
           <p>
-            We are Qatar&apos;s first insurance technology platform designed with Sharia compliance.
-            We offer insurance companies full customisation and control over their data while equipping them with cutting-edge, 
-            regulatory-compliant tools.
+            We are Qatar&apos;s first insurance technology platform designed
+            with Sharia compliance. We offer insurance companies full
+            customisation and control over their data while equipping them with
+            cutting-edge, regulatory-compliant tools.
           </p>
           <p>
-          We&apos;re on a mission to transform insurance with blockchain and
-            AI to create a transparent, ethical  platform that empowers
-            Insurance companies and SMEs to share risk and promotes financial inclusion for all.
+            We&apos;re on a mission to transform insurance with blockchain and
+            AI to create a transparent, ethical platform that empowers Insurance
+            companies and SMEs to share risk and promotes financial inclusion
+            for all.
           </p>
         </div>
       </div>
--- src/components/SubFooter.jsx
+++ src/components/SubFooter.jsx
@@ -1,6 +1,6 @@
 const SubFooter = () => {
   return (
-    <div className="px-4 text-sm py-16 md:px-10 lg:px-20 lg:py-20 lg:text-base xl:py-28 xl:px-[7.5rem]">
+    <div className="px-4 text-sm pt-10 pb-16 md:px-10 lg:px-20 lg:py-20 lg:text-base xl:py-28 xl:px-[7.5rem]">
       <div>
         <h2 className="text-2xl font-semibold text-center leading-7 md:leading-8 md:w-3/5 md:mx-auto lg:text-3xl lg:w-2/3 xl:text-[2.438rem] xl:leading-tight xl:w-1/2">
           Stay ahead of the curve—be the first to hear about our launch!
