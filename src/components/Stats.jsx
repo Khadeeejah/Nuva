3d3acacdbd56ca49d9aa8ab467833a92f672898a
devfiona <87800899+devfiona@users.noreply.github.com>
2024-12-05 07:03:06 +0100

    fix: design bugs

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,7 +6,7 @@ const Stats = () => {
       <p className="font-extralight italic text-center lg:text-lg">
         Did you know that?
       </p>
-      <div className="mt-10 grid gap-20 md:px-16 lg:px-28 xl:px-48">
+      <div className="mt-10 grid gap-20 md:px-16 lg:px-28 xl:px-40">
         <h4 className="text-lg font-medium text-center leading-7 md:text-xl md:leading-8 xl:text-2xl xl:leading-10">
           A 2023 PwC report highlights that 60% of insurers face slow claims
           processing and low customer satisfaction. Scalability, Opaque Policy
@@ -15,7 +15,7 @@ const Stats = () => {
           efficient, and compliant solutions.
         </h4>
         <div className="flex justify-between md:px-12 lg:px-16 xl:px-24">
-          <div className="flex flex-col justify-center items-center gap-1">
+          <div className="flex flex-col justify-center items-center gap-1 lg:gap-2">
             <h3 className="text-4xl text-primary-100 font-medium lg:text-5xl xl:text-6xl">
               <CountUp
                 start={0}
@@ -25,9 +25,11 @@ const Stats = () => {
                 suffix="%"
               />
             </h3>
-            <p className="text-xs md:text-sm lg:text-base">Lorem ipsum dolor</p>
+            <p className="text-neutral-100 text-xs md:text-sm lg:text-base">
+              Lorem ipsum dolor
+            </p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-1">
+          <div className="flex flex-col justify-center items-center gap-1 lg:gap-2">
             <h3 className="text-4xl text-primary-100 font-medium lg:text-5xl xl:text-6xl">
               <CountUp
                 start={0}
@@ -37,9 +39,11 @@ const Stats = () => {
                 suffix="%"
               />
             </h3>
-            <p className="text-xs md:text-sm lg:text-base">Lorem ipsum dolor</p>
+            <p className="text-neutral-100 text-xs md:text-sm lg:text-base">
+              Lorem ipsum dolor
+            </p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-1">
+          <div className="flex flex-col justify-center items-center gap-1 lg:gap-2">
             <h3 className="text-4xl text-primary-100 font-medium lg:text-5xl xl:text-6xl">
               <CountUp
                 start={0}
@@ -49,7 +53,9 @@ const Stats = () => {
                 suffix="%"
               />
             </h3>
-            <p className="text-xs md:text-sm lg:text-base">Lorem ipsum dolor</p>
+            <p className="text-neutral-100 text-xs md:text-sm lg:text-base">
+              Lorem ipsum dolor
+            </p>
           </div>
         </div>
       </div>
